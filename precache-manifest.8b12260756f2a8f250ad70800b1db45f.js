e88c80a32a77f2817f61ab9023979feef9d6359d
Leenawat <leenawat.pa@gmail.com>
2020-04-22 14:44:41 +0700

    'deploy'

---
+++ precache-manifest.8b12260756f2a8f250ad70800b1db45f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76640e8b12c9f74cbbe1d034c661d02b",
+    "url": "/material-ui-thai-datepickers-demo/index.html"
+  },
+  {
+    "revision": "f21d2854ee0b97830cd1",
+    "url": "/material-ui-thai-datepickers-demo/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "cd076fd780a1135c8cad",
+    "url": "/material-ui-thai-datepickers-demo/static/js/2.0a98631e.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/material-ui-thai-datepickers-demo/static/js/2.0a98631e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f21d2854ee0b97830cd1",
+    "url": "/material-ui-thai-datepickers-demo/static/js/main.5d262b1e.chunk.js"
+  },
+  {
+    "revision": "b77e8fe2f2c55c26bbcb",
+    "url": "/material-ui-thai-datepickers-demo/static/js/runtime-main.4dcacb52.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/material-ui-thai-datepickers-demo/precache-manifest.8b12260756f2a8f250ad70800b1db45f.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/material-ui-thai-datepickers-demo/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
